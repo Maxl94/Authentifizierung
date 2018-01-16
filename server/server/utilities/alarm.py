50e28c6352e50000d6f4947a087b688769fdda78
Michael Stadler <27729280+GhostMST@users.noreply.github.com>
2018-01-16 17:49:29 +0100

    fix: add init for mixer, renamed vars, used native busy method

--- server/server/utilities/alarm.py
+++ server/server/utilities/alarm.py
@@ -1,29 +1,27 @@
 import pygame
-import time
 
 
-class Alarm():
+class Alarm:
 
     # sound by soundbible.com http://soundbible.com/1937-Tornado-Siren-II.html
-    mixer = pygame.mixer
-    is_playing = False
     sound_file_path = "/home/pi/Authentifzierung/server/server/utilities/Sirene.wav"
+
+    def __init__(self):
+        pygame.init()
+        self.sound = pygame.mixer.Sound(self.sound_file_path)
 
     def start_alarm(self, duration):
         print("got alarm request")
-        pygame.init()
-        self.mixer = pygame.mixer.Sound(self.sound_file_path)
-        self.is_playing = True
-
-        # play sound
-        print("start alarm")
-        self.mixer.play(maxtime=duration*1000)
+        if not pygame.mixer.get_busy():
+            print("start alarm")
+            self.sound.play(maxtime=duration * 1000)
+        else:
+            print("alarm already playing")
 
     def stop_alarm(self):
         print("got stop alarm request")
-        if self.is_playing:
-            self.mixer.stop()
-            self.is_playing = False
+        if pygame.mixer.get_busy():
+            self.sound.stop()
             print("stopped alarm")
         else:
             print("no active alarm")

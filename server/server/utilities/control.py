35bf4762d690db4926894969984abab1c2f533c7
Michael Stadler <27729280+GhostMST@users.noreply.github.com>
2018-01-23 15:28:52 +0100

    feat: add safe zone dummies and gps diff control

--- server/server/utilities/control.py
+++ server/server/utilities/control.py
@@ -6,15 +6,21 @@ from .hw import ir_modul
 from .hw import motion_det
 import time
 
+
 class Control(Thread):
 
-    def __init__(self, config):
+    def __init__(self, config, dummy_safe_zone):
         print("started init")
         super().__init__()
         self.sensor_timeout = 0.01
         self.ir_threshold = 20
 
+        self.gps_array = [0, 0]
+        self.old_gps_array = [0, 0]
+        self.gps_diff_threshold = 0.0005
+
         self.config = config
+        self.dummy_safe_zone = dummy_safe_zone
 
         self.alarm = Alarm()
         self.gyro = bno.Gyro()
@@ -35,8 +41,9 @@ class Control(Thread):
     # method looks for active sensors and if the want to request an alarm
     def check_sensors(self):
         while True:
-            #print("checking active sensors")
-            #self.config.debug()
+            # print("checking active sensors")
+            # self.config.debug()
+            # ----- alarm sensors ---------
             if self.config.sound_alarm_is_active:
                 if self.config.gyro_sensor_is_active:
                     self.check_sensor_gyro()
@@ -45,15 +52,14 @@ class Control(Thread):
                 # if self.config.light_sensor_is_active:
                     # TODO fix light sensor
                     # self.check_sensor_light()
+
+            # ----- gps sensor ----------
             if self.config.gps_is_active:
                 self.update_gps_data()
-            time.sleep(self.sensor_timeout)
+                if self.check_gps_diff():
+                    self.check_safe_zones()
 
-    # write gps data to django database on regular bases
-    def update_gps_data(self):
-        # TODO call gps sensor for value
-        gps_array = [48.85, 2.29]
-        # TODO overwrite db value with new gps value
+            time.sleep(self.sensor_timeout)
 
     # method to update config, config includes all active sensors and utility
     def update_config(self, config):
@@ -76,6 +82,41 @@ class Control(Thread):
     def stop_alarm(self):
         print("requesting to stop alarm")
         self.alarm.stop_alarm()
+
+    # ----- gps methods -----
+    # write gps data to django database on regular bases
+    def update_gps_data(self):
+        print("updating gps data")
+        self.old_gps_array = self.gps_array
+
+        # TODO Impl real gps check here
+        self.gps_array = [48.85, 2.29]
+
+        self.dummy_safe_zone.x_cord = self.gps_array[0]
+        self.dummy_safe_zone.y_cord = self.gps_array[1]
+
+        self.save()
+
+    def check_gps_diff(self):
+        # TODO get real geo cord comparison with lib
+        old_x_cord_val = abs(self.old_gps_array[0])
+        old_y_cord_val = abs(self.old_gps_array[1])
+        new_x_cord_val = abs(self.gps_array[0])
+        new_y_cord_val = abs(self.gps_array[1])
+
+        x_cord_dif = abs(old_x_cord_val - new_x_cord_val)
+        y_cord_dif = abs(old_y_cord_val - new_y_cord_val)
+
+        if x_cord_dif >= self.gps_diff_threshold or y_cord_dif >= self.gps_diff_threshold:
+            return True
+        else:
+            return False
+
+    def check_safe_zones(self):
+        # TODO check if inside of a safe zone
+        # iterate over safe zone array
+        # maybe sort array first
+        self.gps_array
 
     # ----- sensor methods -----
     def check_sensor_gyro(self):

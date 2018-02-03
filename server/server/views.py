7de661f517d23b8895e9c9ee3e97f20d205ffc6d
Maxl94 <balluff@hm.edu>
2018-02-03 14:07:12 +0100

    Removed "Current Location" Entry from overview

--- server/server/views.py
+++ server/server/views.py
@@ -106,7 +106,7 @@ class LocationsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['locations'] = Safezone.objects.all()
+        context['locations'] = Safezone.objects.filter(id__gte=2)
         print(context['locations'])
         return context
 

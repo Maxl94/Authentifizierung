65c7c44e6a1d8fb1ca972918e0c67c631a7af838
Maxl94 <balluff@hm.edu>
2018-02-03 16:55:36 +0100

    Bugfux

--- server/server/views.py
+++ server/server/views.py
@@ -15,9 +15,8 @@ def nfc_callback(status, id):
         ACTIVE_MODE = 3
     else:
         ACTIVE_MODE = 1
-    obj = Profile.objects.get(id=ACTIVE_MODE)
 
-    ACTIVE_MODE = obj.id
+    obj = Profile.objects.get(id=ACTIVE_MODE)
     CONTROLLER.update_config(obj)
     print('Active mode = {0}'.format(ACTIVE_MODE))
 
